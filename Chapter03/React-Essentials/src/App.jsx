1a37b1baabd47c1a45a79a1adb5b2018034d4101
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-07-28 14:48:43 +0530

    Chapter03: More Props Syntaxes

--- Chapter03/React-Essentials/src/App.jsx
+++ Chapter03/React-Essentials/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js'
 
 const reactDescriptions  = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,12 +21,12 @@ function Header() {
   );
 }
 
-function CoreConcept (props) {
+function CoreConcept ({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt="" />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -40,14 +40,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-            title="components"
-            description="The core UI building block"
-            image={componentImg}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
-
+            <CoreConcept { ...CORE_CONCEPTS[1] } />
+            <CoreConcept { ...CORE_CONCEPTS[2] } />
+            <CoreConcept { ...CORE_CONCEPTS[3] } />
           </ul>
         </section>
       </main>
