cb30f126c0b5aa670d6cce01c04ead432333e1db
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-08-11 17:54:59 +0530

    Chapter03: 57: CSS Styling & Dynamic Styling

--- Chapter03/React-Essentials/src/components/TapButton.jsx
+++ Chapter03/React-Essentials/src/components/TapButton.jsx
@@ -1,7 +1,7 @@
-export default function TapButton ({children, onSelect}) {
+export default function TapButton ({children, onSelect, isSelected}) {
     return (
         <li>
-            <button onClick={onSelect}>
+            <button className = {isSelected ? 'active' : undefined} onClick={onSelect}>
                 { children }
             </button>
         </li>
