0d6e9eb22e0c80dc2faafc854b0671c282fc4e1e
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-08-21 07:47:01 +0530

    Chapter04: 62: Working With Fragments Method 2

--- Chapter04/React-Essentials/src/App.jsx
+++ Chapter04/React-Essentials/src/App.jsx
@@ -13,7 +13,50 @@ function App() {
   }
 
   return (
-    <Fragment>
+    // <Fragment>
+    //   <Header />
+    //   <main>
+    //     <section id="core-concepts">
+    //       <h2>Core Concepts</h2>
+    //       <ul>
+    //         {
+    //           CORE_CONCEPTS.map((conceptItem) => {
+    //             return <CoreConcept key={conceptItem.title} { ...conceptItem } />
+    //           })
+    //         /* <CoreConcept 
+    //         title={CORE_CONCEPTS[0].title}
+    //         description={CORE_CONCEPTS[0].description}
+    //         image={CORE_CONCEPTS[0].image}
+    //         />
+    //         <CoreConcept { ...CORE_CONCEPTS[1] } />
+    //         <CoreConcept { ...CORE_CONCEPTS[2] } />
+    //         <CoreConcept { ...CORE_CONCEPTS[3] } /> */}
+    //       </ul>
+    //     </section>
+    //     <section id="examples">
+    //       <h2>Examples</h2>
+    //       <menu>
+    //         <TapButton isSelected={selectedTopic === 'components'} onSelect={() => { handleSelect('components') }}>Components</TapButton>
+    //         <TapButton isSelected={selectedTopic === 'jsx'} onSelect={() => { handleSelect('jsx') }}>JSX</TapButton>
+    //         <TapButton isSelected={selectedTopic === 'props'} onSelect={() => { handleSelect('props') }}>Props</TapButton>
+    //         <TapButton isSelected={selectedTopic === 'state'} onSelect={() => { handleSelect('state') }}>State</TapButton>
+    //       </menu>
+    //       {!selectedTopic ? (<p>Please select a topic.</p>) :
+    //         (<div id='tab-content'>
+    //           <h3>{EXAMPLES[selectedTopic].title}</h3>
+    //           <p>{EXAMPLES[selectedTopic].description}</p>
+    //           <pre>
+    //             <code>
+    //               {EXAMPLES[selectedTopic].code}
+    //             </code>
+    //           </pre>
+    //         </div>)
+    //       }
+    //     </section>
+    //   </main>
+    // </Fragment>
+
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -21,7 +64,7 @@ function App() {
           <ul>
             {
               CORE_CONCEPTS.map((conceptItem) => {
-                return <CoreConcept key={conceptItem.title} { ...conceptItem } />
+                return <CoreConcept key={conceptItem.title} {...conceptItem} />
               })
             /* <CoreConcept 
             title={CORE_CONCEPTS[0].title}
@@ -54,7 +97,7 @@ function App() {
           }
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
