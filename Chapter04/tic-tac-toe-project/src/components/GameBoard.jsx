a5b646e263e2664dcbea46c20447f1cc9e7e47e5
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-09-16 06:36:58 +0530

    Chapter04: 81: Best Practice: Updating Object State Immutably

--- Chapter04/tic-tac-toe-project/src/components/GameBoard.jsx
+++ Chapter04/tic-tac-toe-project/src/components/GameBoard.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
-];
+    [null, null, null]];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare (rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            return updatedGameBoard;
+        })
+    }
+
     return (
         <ol id="game-board">
             {
-                initialGameBoard.map((row, rowIndex) => {
+                gameBoard.map((row, rowIndex) => {
                     return (<li key={rowIndex}>
                         <ol>
                             {row.map((playerSymbol, colIndex) => {
                                 return (<li key={colIndex}>
-                                    <button>
+                                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
                                         {playerSymbol}
                                     </button>
                                 </li>);
