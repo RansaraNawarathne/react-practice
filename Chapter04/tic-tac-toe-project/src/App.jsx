8f6d65c7a7735167cafac27bc5d2fbf363468341
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-09-18 07:29:18 +0530

    Chapter04: 90: Lifting Computed Values up

--- Chapter04/tic-tac-toe-project/src/App.jsx
+++ Chapter04/tic-tac-toe-project/src/App.jsx
@@ -4,6 +4,11 @@ import Player from "./components/Player.jsx"
 import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]];
+
 function deriveActivePlayer (gameTurns) {
   let currentPlayer = 'X';
 
@@ -19,6 +24,21 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    // const firstSquareSymbol
+    // const secondSquareSymbol
+    // const thirdSquareSymbol
+  }
 
   function handleSelectSquare (rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' );
@@ -41,7 +61,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
     </main>
