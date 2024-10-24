312c4abd68d7b31d9e66eb7d48346b399640239f
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-10-25 00:47:41 +0530

    Chapter05: 97: Created Header Component and Initial Commit of Chapter05

---
+++ Chapter05/Malindu/react-essentials-practice/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import investmentCalculatorLogoImg from './../assets/investment-calculator-logo.png'
+
+export default function Header () {
+    return (
+        <dir id="header">
+            <img src={investmentCalculatorLogoImg} alt="Investment Calculator Logo" />
+            <h1>React Investment Calculator</h1>
+        </dir>
+    );
+}
