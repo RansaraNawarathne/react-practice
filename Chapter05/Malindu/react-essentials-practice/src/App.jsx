244883bedaf4ec9bcb36beda381ec29e2368e666
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-11-03 12:26:41 +0530

    Chapter05: 97: implemented and integrated result component

--- Chapter05/Malindu/react-essentials-practice/src/App.jsx
+++ Chapter05/Malindu/react-essentials-practice/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import InputSection from "./components/InputSection"
+import Result from "./components/Result"
 
 function App() {
   return (
     <>
       <Header />
       <InputSection />
+      <Result />
     </>
   )
 }
