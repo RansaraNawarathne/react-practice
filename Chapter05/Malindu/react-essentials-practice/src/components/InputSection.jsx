2b40a772e7f63f0c38e228bd49625d2f087ede0b
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-11-03 12:17:02 +0530

    Chapter05: 97: integrated useState hook to manage investment data

--- Chapter05/Malindu/react-essentials-practice/src/components/InputSection.jsx
+++ Chapter05/Malindu/react-essentials-practice/src/components/InputSection.jsx
@@ -1,27 +1,44 @@
+import { useState } from "react";
 
 export default function InputSection() {
+    const [investmentData, setInvestmentData] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 5000,
+        expectedReturn: 50000,
+        duration: 10
+    });
+
+    function handleInvestmentDataChanges(valueKey, value) {
+        setInvestmentData((prevData) => {
+            return {
+                ...prevData,
+                [valueKey]: value
+            }
+        });
+    }
+
     return (
-        <div id="user-input">
+        <section id="user-input">
             <div className="input-group">
                 <p>
                     <label htmlFor="txtInitialInvestment">Initial Investment</label>
-                    <input type="text" name="txtInitialInvestment" />
+                    <input type="text" name="txtInitialInvestment" required value={investmentData.initialInvestment} onChange={(event) => handleInvestmentDataChanges('initialInvestment', event.target.value)} />
                 </p>
                 <p>
                     <label htmlFor="txtAnnualInvestment">Annual Investment</label>
-                    <input type="text" name="txtAnnualInvestment" />
+                    <input type="text" name="txtAnnualInvestment" required value={investmentData.annualInvestment} onChange={(event) => handleInvestmentDataChanges('annualInvestment', event.target.value)} />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label htmlFor="txtExpectedReturn">Expected Return</label>
-                    <input type="text" name="txtExpectedReturn" />
+                    <input type="text" name="txtExpectedReturn" required value={investmentData.expectedReturn} onChange={(event) => handleInvestmentDataChanges('expectedReturn', event.target.value)} />
                 </p>
                 <p>
                     <label htmlFor="txtDuration">Duration</label>
-                    <input type="number" name="txtDuration" />
+                    <input type="number" name="txtDuration" required value={investmentData.duration} onChange={(event) => handleInvestmentDataChanges('duration', event.target.value)} />
                 </p>
             </div>
-        </div>
+        </section>
     );
 }
