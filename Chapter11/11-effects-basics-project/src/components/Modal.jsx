7eccb583626b178bcfe1d700fbd35cf650a23bdc
Malindu Nawarathne <ransaranawarathne@gmail.com>
2025-06-10 13:18:45 +0530

    Chapter11: 186: Fixing a Small Bug

--- Chapter11/11-effects-basics-project/src/components/Modal.jsx
+++ Chapter11/11-effects-basics-project/src/components/Modal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 // const Modal = forwardRef(function Modal({ children }, ref) {
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   /* useImperativeHandle(ref, () => {
@@ -25,7 +25,7 @@ function Modal({ open, children }) {
   }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
